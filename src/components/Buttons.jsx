1d8d6094158a00f62bebb271372c6534e97b8c66
Dipesh <dipeshgoud64@gmail.com>
2024-04-21 01:55:11 +0530

    second

--- src/components/Buttons.jsx
+++ src/components/Buttons.jsx
@@ -1,10 +1,10 @@
 import React from "react";
+import ButtonSvg from "../assets/svg/ButtonSvg"
+
 
 const Buttons = ({ className, href, onClick, children, px, white }) => {
 
-  const classes = `button relative inline-flex item-center justify-center h-11 transition-colors hover:text-color-1 ${
-    px || "px-7"
-  } ${white ? "text-n-8" : "text-n-1"} ${className || ""}`;
+  const classes = `button relative inline-flex items-center justify-center h-11 transition-colors hover:text-color-1 ${px || "px-7"} ${white ? "text-n-8" : "text-n-1"} ${className || ""}`;
 
   const spanClasses = "relative z-10";
 
@@ -14,6 +14,15 @@ const Buttons = ({ className, href, onClick, children, px, white }) => {
         {ButtonSvg(white)}
     </button>
   )
+
+  const renderLink = () => (
+    <a href={href} className={classes}>
+        <span className={spanClasses}>{children}</span>
+        {ButtonSvg(white)}
+    </a>
+  )
+
+  return href ? renderLink() : renderButton();
 };
 
 export default Buttons;
---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+ import React from 'react'
+ 
+ const Header = () => {
+   return (
+     <div className='fixed top-0 z-50'>
+       
+     </div>
+   )
+ }
+ 
+ export default Header
+ 
